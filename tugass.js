2bebabb47be3732c33482dbfeb334978c708f370
Riri Triana <riritriana21@gmail.com>
2023-10-17 16:44:38 +0700

    tugas Export-Import

---
+++ tugass.js
@@ -0,0 +1,4 @@
+export const panggil = {
+    nama:["Riri Triana", "Ades Roydatul ","Raja Salsabila"],
+    nim:[123, 456, 789]
+ }
---
+++ tugass1.js
@@ -0,0 +1,8 @@
+import{panggil} from "./tugass.js";
+
+panggil.nama.forEach((x,y )=> {
+    console.log(`Nama\t:${x}`);
+    console.log(`NIM\t: ${panggil.nim[y]}`);
+    console.log();
+});
+
